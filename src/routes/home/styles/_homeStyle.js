2bd09264fb1db90a3f82f200524cb65324d47e18
ridoansaleh <ridoansalehnasution98@gmail.com>
2019-12-21 22:43:56 +0700

    add style for other than desktop device

--- src/routes/home/styles/_homeStyle.js
+++ src/routes/home/styles/_homeStyle.js
@@ -1,21 +1,27 @@
 import styled from 'styled-components';
+import { BREAKPOINTS } from '../../../utils/devices';
 
 export const PageWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
-  //   background: red;
+  width: 100%;
+  padding: 10px;
   background-image: linear-gradient(red, white);
-  padding: 40px;
-  min-height: 1030px;
-  min-width: 580px;
+  box-sizing: border-box;
+  @media only screen and (min-width: ${BREAKPOINTS.DESKTOP}) {
+    width: 100%;
+    min-height: 1030px;
+  }
 `;
 
 export const Content = styled.div`
   height: 950px;
-  width: 500px;
-  border: 20px solid black;
+  width: 100%;
   border-radius: 35px;
   background: white;
+  @media only screen and (min-width: ${BREAKPOINTS.DESKTOP}) {
+    border: 20px solid black;
+    width: 500px;
+  }
 `;
