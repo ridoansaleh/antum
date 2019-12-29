655a9770fffd8efc9acdefc6a6a126afb3f0cbf1
RSN <ridoansalehnasution98@gmail.com>
2019-12-29 21:28:02 +0700

    implement tablet breakpoints etc (#9)

--- src/routes/home/styles/_homeStyle.js
+++ src/routes/home/styles/_homeStyle.js
@@ -6,11 +6,11 @@ export const PageWrapper = styled.div`
   justify-content: center;
   align-items: center;
   width: 100%;
+  min-width: 320px;
   padding: 10px;
   background-image: linear-gradient(red, white);
   box-sizing: border-box;
   @media only screen and (min-width: ${BREAKPOINTS.DESKTOP}) {
-    width: 100%;
     min-height: 1030px;
   }
 `;
@@ -20,6 +20,12 @@ export const Content = styled.div`
   width: 100%;
   border-radius: 35px;
   background: white;
+  @media only screen and (min-width: ${BREAKPOINTS.PORTRAIT_TABLET}) {
+    width: 80%;
+  }
+  @media only screen and (min-width: ${BREAKPOINTS.TABLET}) {
+    width: 70%;
+  }
   @media only screen and (min-width: ${BREAKPOINTS.DESKTOP}) {
     border: 20px solid black;
     width: 500px;
