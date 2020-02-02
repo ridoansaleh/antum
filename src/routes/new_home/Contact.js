c17e43edd2c1c1c379f614839161ffab755ad9f1
ridoansaleh <ridoansalehnasution98@gmail.com>
2020-02-02 10:23:57 +0700

    add hobby and contact sections

--- src/routes/new_home/Contact.js
+++ src/routes/new_home/Contact.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container, FieldWrapper, Input, Label, TextArea, Button } from './styles/_contactStyle';
 
 function Contact() {
-  return <div>Contact</div>;
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+  const handleChangeEmail = e => {
+    setEmail(e.target.value);
+  };
+  const handleChangeSubject = e => {
+    setSubject(e.target.value);
+  };
+  const handleChangeMessage = e => {
+    setMessage(e.target.value);
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+  return (
+    <Container>
+      <form onSubmit={handleSubmit}>
+        <FieldWrapper>
+          <Label>Email:</Label>
+          <Input type="text" value={email} onChange={handleChangeEmail} />
+        </FieldWrapper>
+        <FieldWrapper>
+          <Label>Subject</Label>
+          <Input type="text" value={subject} onChange={handleChangeSubject} />
+        </FieldWrapper>
+        <FieldWrapper>
+          <Label>Message</Label>
+          <TextArea value={message} onChange={handleChangeMessage} />
+        </FieldWrapper>
+        <Button type="submit" value="Send" />
+      </form>
+    </Container>
+  );
 }
 
 export default Contact;
